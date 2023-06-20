00533a5e08362c14e707d2d50ea573a21935fe4f
Mike <mjKosmic@gmail.com>
2023-06-19 22:53:55 -0500

    added ability for bars to grab a second page if a next_page_token was in the response

--- Sources/Alpaca/AlpacaDataClient+Bars.swift
+++ Sources/Alpaca/AlpacaDataClient+Bars.swift
@@ -44,10 +44,20 @@ public struct Bar: Codable {
     public var volWeightedAvgPrice: Double { vw }
 }
 
+struct BarResponse: Codable {
+    let bars: [String: [Bar]]
+    let nextPageToken: String?
+    
+    enum CodingKeys: String, CodingKey {
+        case bars
+        case nextPageToken = "next_page_token"
+    }
+}
+
 extension AlpacaDataClient {
 
-    public func bars(_ timeframe: Bar.Timeframe, symbols: [String], limit: Int? = nil, start: Date? = nil, end: Date? = nil, after: Date? = nil, until: Date? = nil) async throws -> [String: [String: [Bar]]] {
-        return try await get("stocks/bars", searchParams: [
+    public func bars(_ timeframe: Bar.Timeframe, symbols: [String], limit: Int? = nil, start: Date? = nil, end: Date? = nil, after: Date? = nil, until: Date? = nil) async throws -> [String: [Bar]] {
+        var searchParams: HTTPSearchParams = [
             "symbols": symbols.joined(separator: ","),
             "limit": limit.map(String.init),
             "start": start.map(Utils.iso8601DateOnlyFormatter.string),
@@ -55,16 +65,27 @@ extension AlpacaDataClient {
             "after": after.map(Utils.iso8601DateFormatter.string),
             "until": until.map(Utils.iso8601DateFormatter.string),
             "timeframe": timeframe.rawValue
-        ])
+        ]
+        let response: BarResponse = try await get("stocks/bars", searchParams: searchParams)
+        var bars = response.bars
+        if let pageToken = response.nextPageToken {
+            searchParams["page_token"] = pageToken
+            let page2: BarResponse = try await get("stocks/bars", searchParams: searchParams)
+            page.bars.forEach {
+                bars[$0]?.append(contentsOf: $1)
+            }
+        }
+        
+        return bars
     }
 
     public func bars(_ timeframe: Bar.Timeframe, symbol: String, limit: Int? = nil, start: Date? = nil, end: Date? = nil, after: Date? = nil, until: Date? = nil) async throws -> [Bar] {
         let res = try await bars(timeframe, symbols: [symbol], limit: limit, start: start, end: end, after: after, until: until)
-        return res["bars", default: [:]][symbol, default: []]
+        return res[symbol, default: []]
     }
 
     public func bars(_ timeframe: Bar.Timeframe, assets: [Asset], limit: Int? = nil, start: Date? = nil, end: Date? = nil, after: Date? = nil, until: Date? = nil) async throws -> [String: [Bar]] {
-        return try await bars(timeframe, symbols: assets.map(\.symbol), limit: limit, start: start, end: end, after: after, until: until)["bars", default: [:]]
+        return try await bars(timeframe, symbols: assets.map(\.symbol), limit: limit, start: start, end: end, after: after, until: until)
     }
 
     public func bars(_ timeframe: Bar.Timeframe, asset: Asset, limit: Int? = nil, start: Date? = nil, end: Date? = nil, after: Date? = nil, until: Date? = nil) async throws -> [Bar] {
