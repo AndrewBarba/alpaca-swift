a4290cd329dc6aebc984ae1d0906cb2552f0277d
Mike <mjKosmic@gmail.com>
2023-06-25 16:21:03 -0500

    trying to fix date formatter

--- Sources/Alpaca/Utils.swift
+++ Sources/Alpaca/Utils.swift
@@ -14,7 +14,7 @@ internal enum Utils {
         formatter.calendar = Foundation.Calendar(identifier: .iso8601)
         formatter.locale = Locale(identifier: "en_US_POSIX")
         formatter.timeZone = TimeZone(secondsFromGMT: 0)
-        formatter.dateFormat = "yyyy-MM-dd'T'HH:mm:ssZ"
+        formatter.dateFormat = "yyyy-MM-dd'T'HH:mm:ssZZZZZ"
         return formatter
     }()
 
