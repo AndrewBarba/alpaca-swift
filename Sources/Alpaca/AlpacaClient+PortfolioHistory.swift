baf45909dde1419cb5e41f2890424954c82f6d9d
mike mello <mjKosmic@gmail.com>
2023-12-10 13:40:22 -0600

    manually calculating pl and pl percent values for portfolio history because of an existing known issue with that endpoint

--- Sources/Alpaca/AlpacaClient+PortfolioHistory.swift
+++ Sources/Alpaca/AlpacaClient+PortfolioHistory.swift
@@ -7,6 +7,9 @@
 
 import Foundation
 
+/*
+ This struct is unusable as-is because there is a known issue with the portfolio history endpoint that leads to incorrect PL and PL% calculations
+ so we will calculate those ourselves
 public struct PortfolioHistory: Codable {
     public enum Timeframe: String, Codable, CaseIterable {
         case oneMin = "1Min"
@@ -30,6 +33,59 @@ public struct PortfolioHistory: Codable {
         self.profitLossPct = profitLossPct
         self.baseValue = baseValue
         self.timeframe = timeframe
+    }
+}
+ */
+public struct PortfolioHistory: Decodable {
+    public enum CodingKeys: String, CodingKey {
+        case timestamp
+        case baseValue
+        case timeframe
+        case equity
+        case profitLoss
+        case profitLossPct
+    }
+    public enum Timeframe: String, Codable, CaseIterable {
+        case oneMin = "1Min"
+        case fiveMin = "5Min"
+        case fifteenMin = "15Min"
+        case oneHour = "1H"
+        case oneDay = "1D"
+    }
+    public struct Value {
+        public let timestamp: TimeInterval
+        public let equity: Double
+        public let profitLoss: Double
+        public let profitLossPct: Double
+    }
+    public let values: [Value]
+    public let baseValue: Double
+    public let timeframe: Timeframe
+    
+    public init(values: [Value], baseValue: Double, timeframe: Timeframe) {
+        self.values = values
+        self.baseValue = baseValue
+        self.timeframe = timeframe
+    }
+    
+    public init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        self.baseValue = try container.decode(Double.self, forKey: .baseValue)
+        self.timeframe = try container.decode(Timeframe.self, forKey: .timeframe)
+        
+        let timestamps = try container.decode([TimeInterval].self, forKey: .timestamp)
+        let equities = try container.decode([Double?].self, forKey: .equity)
+        var values: [Value] = []
+        
+        for i in 0..<timestamps.count {
+            if let equity = equities[i] {
+                let pl = equity - baseValue
+                let plPct = pl / baseValue
+                values.append(.init(timestamp: timestamps[i], equity: equity, profitLoss: pl, profitLossPct: plPct))
+            }
+        }
+        
+        self.values = values
     }
 }
 
