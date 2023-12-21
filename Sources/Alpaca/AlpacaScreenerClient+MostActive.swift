be5dd0d3653b8d919c8276ff3be9ec5c36e5d9f0
mike mello <mjKosmic@gmail.com>
2023-12-21 13:42:52 -0600

    added caseiterable to MostActive.Metric

--- Sources/Alpaca/AlpacaScreenerClient+MostActive.swift
+++ Sources/Alpaca/AlpacaScreenerClient+MostActive.swift
@@ -8,7 +8,7 @@
 import Foundation
 
 public struct MostActive: Codable {
-    public enum Metric: String, Codable {
+    public enum Metric: String, Codable, CaseIterable {
         case volume
         case trades
     }
