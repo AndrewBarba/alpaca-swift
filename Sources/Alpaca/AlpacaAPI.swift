2568eae2f98631fdb2f4c2744fecd19aae6760ca
mike mello <mjKosmic@gmail.com>
2024-01-21 18:40:49 -0600

    made environment parameter nameless

--- Sources/Alpaca/AlpacaAPI.swift
+++ Sources/Alpaca/AlpacaAPI.swift
@@ -12,7 +12,7 @@ public struct AlpacaAPI {
     public var trading: AlpacaTradingClient
     public var data: AlpacaDataClient
     
-    public init(environment: Environment, timeoutInterval: TimeInterval = 10) {
+    public init(_ environment: Environment, timeoutInterval: TimeInterval = 10) {
         self.trading = AlpacaTradingClient(environment: environment, timeoutInterval: timeoutInterval)
         self.data = AlpacaDataClient(authType: environment.authType(), timeoutInterval: timeoutInterval)
     }
